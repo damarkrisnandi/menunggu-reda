0c6f1a18c83cf1d3e532f3f5e67c27400c95f9e9
damar <damar@gitlab.lab>
2022-08-17 15:42:14 +0700

    setting utils for hidden

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
+
+import utils from './utils/utils.json';
 import Family from './components/Family';
 import Header from './components/Header';
 import Date from './components/Date';
@@ -8,16 +10,27 @@ import Timeline from './components/Timeline';
 import Footer from './components/Footer';
 import Theme from './utils/theme';
 import GmapsLink from './components/GmapsLink';
+
+function show(component, id) {
+  if (!utils.hidden.includes(id)) {
+    return (
+      component
+    )
+  }
+
+  return null;
+}
+
 function App() {
   const theme = new Theme('light');
   return (
     <div className={`App ${theme.bgColor}`}>
       <Header theme={theme} />
-      <Family theme={theme} />
-      <Date theme={theme}/>
-      <Countdown theme={theme}/>
-      <Timeline theme={theme}/>
-      <GmapsLink theme={theme}/>
+      { show(<Family theme={theme} />, 'family') }
+      { show(<Date theme={theme}/>, 'date') }
+      { show(<Countdown theme={theme}/>, 'countdown') }
+      { show(<Timeline theme={theme}/>, 'timeline') }
+      { show(<GmapsLink theme={theme}/>, 'gmaps') }
       <Footer theme={theme}/>
       
     </div>
